9c14f49cf3ea723062c17060621190cbdede1a5c
KaratekHD <karatekhd@opensuse.org>
2022-12-27 18:22:10 +0100

    improve spacing between headlines

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,29 +1,29 @@
 <template>
   <v-container>
-    <h1 class="text-h2">How to contact me</h1><br>
+    <h1 class="text-h2">How to contact me</h1>
     My contact information is linked down in the footer.<br>
     When contacting me, please keep in mind that only Threema and Matrix are encrypted. If what you want do discuss is
     half-way important, please contact me on these platforms.<br><br>
 
-    <h1 class="text-h2">Legal stuff</h1><br>
-    I know, this is borring, but it has to be here.<br>
-    <h1 class="text-h3">Accountability for content</h1><br>
+    <h1 class="text-h2">Legal stuff</h1>
+    I know, this is borring, but it has to be here.
+    <h1 class="text-h3">Accountability for content</h1>
     The contents of our pages have been created with the utmost care. However, we cannot guarantee the contents’
     accuracy, completeness or topicality. According to statutory provisions, we are furthermore responsible for our own
     content on these web pages. In this matter, please note that we are not obliged to monitor the transmitted or saved
     information of third parties, or investigate circumstances pointing to illegal activity. Our obligations to remove
     or block the use of information under generally applicable laws remain unaffected by this as per §§ 8 to 10 of the
-    Telemedia Act (TMG).<br>
-    <h1 class="text-h3">Accountability for links</h1><br>
+    Telemedia Act (TMG).
+    <h1 class="text-h3">Accountability for links</h1>
     Responsibility for the content of external links (to web pages of third parties) lies solely with the operators of
     the linked pages. No violations were evident to us at the time of linking. Should any legal infringement become
     known to us, we will remove the respective link immediately.
-    <h1 class="text-h3">Copyright</h1><br>
+    <h1 class="text-h3">Copyright</h1>
     Our web pages and their contents are subject to German copyright law. Unless expressly permitted by law, every form
     of utilizing, reproducing or processing works subject to copyright protection on our web pages requires the prior
     consent of the respective owner of the rights. Individual reproductions of a work are only allowed for private use.
     The materials from these pages are copyrighted and any unauthorized use may violate copyright laws.
-  <br><br>
+    <br><br>
     <i>Source: translate-24h.de</i>
   </v-container>
 </template>
@@ -33,8 +33,14 @@
 </script>
 
 <style scoped>
-.social-button {
-  margin-right: 5px;
+.text-h3 {
+  margin-bottom: 1rem;
+  margin-top: 1.5rem;
+}
+
+.text-h2 {
+  margin-bottom: 1rem;
+  margin-top: 1.5rem;
 }
 
 </style>
